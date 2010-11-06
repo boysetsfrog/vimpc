f1cbabfae38ec5e282ed3c425f7dd67284d75daf
boysetsfrog <boysetsfrog@fc90b29e-3040-4360-8be2-b397c2834534>
2010-11-06 10:57:49 +0000

    Added GNU header to source files
    git-svn-id: https://vimpc.svn.sourceforge.net/svnroot/vimpc/trunk@6 fc90b29e-3040-4360-8be2-b397c2834534

--- src/dbc.hpp
+++ src/dbc.hpp
@@ -1,8 +1,27 @@
+/*
+   Vimpc
+   Copyright (C) 2010 Nathan Sweetman
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   dbc.hpp - provides error checking and verification 
+   */
+
 #ifndef __DBC
 #define __DBC
 
 #ifdef _DEBUG
-
 #define ASSERT(_expression)  if (!(_expression)) assert_failed(__FILE__, __LINE__)
 #define REQUIRE(_expression) ASSERT(_expression)
 #define ENSURE(_expression)  ASSERT(_expression)
@@ -13,7 +32,6 @@ template<bool> struct static_assert_failed;
 template<>     struct static_assert_failed<true> {};
 
 #else
-
 #define ASSERT(ignore)        ((void) 0)
 #define REQUIRE(ignore)       ((void) 0)
 #define ENSURE(ignore)        ((void) 0)
@@ -22,7 +40,6 @@ template<>     struct static_assert_failed<true> {};
 #endif
 
 #include "stdlib.h"
-
 namespace DBC
 {
    // Ensure that any null pointers cause asserts rather than seg fault
