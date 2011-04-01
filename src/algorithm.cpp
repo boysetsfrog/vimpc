9a151c77f511408eb6160123932fab5a01995f52
boysetsfrog <boysetsfrog@fc90b29e-3040-4360-8be2-b397c2834534>
2011-04-01 14:42:42 +0000

    Forgot to add algorithm class
    git-svn-id: https://vimpc.svn.sourceforge.net/svnroot/vimpc/trunk@135 fc90b29e-3040-4360-8be2-b397c2834534

---
+++ src/algorithm.cpp
@@ -0,0 +1,42 @@
+/*
+   Vimpc
+   Copyright (C) 2010 Nathan Sweetman
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   algorithm.cpp - 
+   */
+
+#include "algorithm.hpp"
+
+bool Algorithm::isLower(std::string const & s1)
+{
+   return isCase(s1, ::tolower);
+}
+
+bool Algorithm::isUpper(std::string const & s1)
+{
+   return isCase(s1, ::toupper);
+}
+
+bool Algorithm::iequals(std::string const & s1, std::string const & s2)
+{
+   std::string lower1(s1);
+   std::string lower2(s2);
+
+   std::transform(lower1.begin(), lower1.end(), lower1.begin(), ::tolower);
+   std::transform(lower2.begin(), lower2.end(), lower2.begin(), ::tolower);
+
+   return (lower1 == lower2);
+}
---
+++ src/algorithm.hpp
@@ -0,0 +1,45 @@
+/*
+   Vimpc
+   Copyright (C) 2010 Nathan Sweetman
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   algorithm.hpp - 
+   */
+
+#ifndef __MAIN__ALGORITHM
+#define __MAIN__ALGORITHM
+
+#include <algorithm>
+#include <string>
+
+namespace Algorithm
+{
+   template <class T>
+   bool isCase(std::string const & s1, T op);
+   bool isLower(std::string const & s1);
+   bool isUpper(std::string const & s1);
+   bool iequals(std::string const & s1, std::string const & s2);
+}
+
+
+template <class T>
+bool Algorithm::isCase(std::string const & s1, T op)
+{
+   std::string case1(s1);
+   std::transform(case1.begin(), case1.end(), case1.begin(), op);
+   return (s1 == case1);
+}
+
+#endif
