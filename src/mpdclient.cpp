4709014945e9126305ff8772d62edcd73a13a8a6
richo <richo@psych0tik.net>
2011-09-13 20:57:49 +1000

    Allow manual specification of host to connect
    Allow user to specific vimpc -h hostname -p port to connect to a given
    host.
    
    Also allow the MPD_HOST and MPD_PORT environemnt variables, in the order
    of
    
    config file
    command line switches
    environment
    defaults (localhost:6600)
    
    I am open to discussion about whether config file should override them.
    
    Also, fix another segfault when it can't connect.

--- src/mpdclient.cpp
+++ src/mpdclient.cpp
@@ -69,15 +69,28 @@ Client::~Client()
 void Client::Connect(std::string const & hostname, uint16_t port)
 {
    DeleteConnection();
+   char *   tmp_env;
+   char *   connect_hostname;
+   uint16_t connect_port;
 
-   connection_ = mpd_connection_new(hostname.c_str(), port, 0);
+    if ((tmp_env = getenv("MPD_HOST")) != NULL)
+        connect_hostname = tmp_env;
+    else
+        connect_hostname = (char *)hostname.c_str();
+
+    if ((tmp_env = getenv("MPD_PORT")) != NULL)
+       connect_port     = atoi(tmp_env);
+    else
+       connect_port     = port;
+
+   connection_ = mpd_connection_new(connect_hostname, connect_port, 0);
+   CheckError();
 
    // Must redraw the library first
    screen_.Redraw(Ui::Screen::Library);
    screen_.Redraw(Ui::Screen::Browse);
 
    CheckForUpdates();
-   CheckError();
 }
 
 void Client::Play(uint32_t const playId)
