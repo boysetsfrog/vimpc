boysetsfrog/vimpc

commit 17663d635530b77acb4b984178e20f15bd0bab09

Added very basic list window for libmpdclient versions above 2.5.0
git-svn-id: https://vimpc.svn.sourceforge.net/svnroot/vimpc/trunk@258 fc90b29e-3040-4360-8be2-b397c2834534

src/mpdclient.hpp
@@ -114,6 +114,9 @@ namespace Mpc
       template <typename Object>
       void ForEachLibrarySong(Object & object, void (Object::*callBack)(Mpc::Song *));
 
+      template <typename Object>
+      void ForEachPlaylist(Object & object, void (Object::*callBack)(std::string));
+
    private:
       unsigned int QueueVersion();
       Song * CreateSong(uint32_t id, mpd_song const * const) const;
@@ -196,6 +199,26 @@ namespace Mpc
       }
    }
 
+   //
+   template <typename Object>
+   void Client::ForEachPlaylist(Object & object, void (Object::*callBack)(std::string))
+   {
+#if LIBMPDCLIENT_CHECK_VERSION(2,5,0)
+      if (Connected() == true)
+      {
+         mpd_send_list_playlists(connection_);
+
+         mpd_playlist * nextPlaylist = mpd_recv_playlist(connection_);
+
+         for(; nextPlaylist != NULL; nextPlaylist = mpd_recv_playlist(connection_))
+         {
+            std::string const playlist = mpd_playlist_get_path(nextPlaylist);
+            (object.*callBack)(playlist);
+            mpd_playlist_free(nextPlaylist);
+         }
+      }
+   }
+#endif
 }
 
 #endif

Makefile.am
@@ -36,6 +36,7 @@ vimpc_SOURCES    = src/algorithm.cpp \
                    src/buffer/buffer.hpp \
                    src/buffer/library.cpp \
                    src/buffer/library.hpp \
+                   src/buffer/list.hpp \
                    src/buffer/playlist.hpp \
                    src/mode/command.cpp \
                    src/mode/command.hpp \
@@ -55,6 +56,8 @@ vimpc_SOURCES    = src/algorithm.cpp \
                    src/window/help.hpp \
                    src/window/librarywindow.cpp \
                    src/window/librarywindow.hpp \
+                   src/window/listwindow.cpp \
+                   src/window/listwindow.hpp \
                    src/window/modewindow.cpp \
                    src/window/modewindow.hpp \
                    src/window/playlistwindow.cpp \

doc/vimpcrc.example
@@ -12,4 +12,7 @@ set window browse
 " Don't stop playing music when we quit
 set nostoponquit
 
+" If you connect in the config file you should try and do it as late as possible
+" connect somehost someport
+
 echo Config File Complete

src/buffer/list.hpp
@@ -1,6 +1,6 @@
 /*
    Vimpc
-   Copyright (C) 2010 Nathan Sweetman
+   Copyright (C) 2010 - 2011 Nathan Sweetman
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,28 +15,32 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-   buffers.hpp - global access to important buffers (playlist, library, console)
+   list.hpp - handling of multiple mpd playlists
    */
 
-#ifndef __MAIN__BUFFERS
-#define __MAIN__BUFFERS
+#ifndef __MPC__LISTS
+#define __MPC__LISTS
 
-#include <iostream>
+// Includes
+#include "buffer.hpp"
+#include "callback.hpp"
 
-#include "window/console.hpp"
-#include "buffer/browse.hpp"
-#include "buffer/library.hpp"
-#include "buffer/list.hpp"
-#include "buffer/playlist.hpp"
-
-namespace Main
+// Lists
+namespace Mpc
 {
-   Mpc::Playlist & Playlist();
-   Mpc::Playlist & PlaylistPasteBuffer();
-   Mpc::Browse   & Browse();
-   Mpc::Library  & Library();
-   Mpc::Lists    & Lists();
-   Ui::Console   & Console();
-}
+   class Lists : public Main::Buffer<std::string>
+   {
+   private:
+      typedef Main::CallbackObject<Mpc::Lists, Lists::BufferType> CallbackObject;
+      typedef Main::CallbackFunction<Lists::BufferType> CallbackFunction;
 
+   public:
+      Lists()
+      {
+      }
+      ~Lists()
+      {
+      }
+   };
+}
 #endif

src/buffers.cpp
@@ -44,6 +44,12 @@ Mpc::Library & Main::Library()
    return *buffer;
 }
 
+Mpc::Lists & Main::Lists()
+{
+   static Mpc::Lists * buffer = new Mpc::Lists();
+   return *buffer;
+}
+
 Ui::Console & Main::Console()
 {
    static Ui::Console * buffer = new Ui::Console();

src/mode/command.cpp
@@ -77,6 +77,7 @@ Command::Command(Ui::Screen & screen, Mpc::Client & client, Main::Settings & set
    commandTable_["help"]      = &Command::SetActiveAndVisible<Ui::Screen::Help>;
    commandTable_["library"]   = &Command::SetActiveAndVisible<Ui::Screen::Library>;
    commandTable_["playlist"]  = &Command::SetActiveAndVisible<Ui::Screen::Playlist>;
+   commandTable_["lists"]     = &Command::SetActiveAndVisible<Ui::Screen::Lists>;
 
    commandTable_["load"]  = &Command::LoadPlaylist;
    commandTable_["save"]  = &Command::SavePlaylist;

src/mpdclient.cpp
@@ -120,6 +120,7 @@ void Client::Connect(std::string const & hostname, uint16_t port)
       // Must redraw the library first
       screen_.Redraw(Ui::Screen::Library);
       screen_.Redraw(Ui::Screen::Browse);
+      screen_.Redraw(Ui::Screen::Lists);
 
       // This will redraw the playlist window
       CheckForUpdates();

src/screen.cpp
@@ -38,6 +38,7 @@
 #include "window/error.hpp"
 #include "window/help.hpp"
 #include "window/librarywindow.hpp"
+#include "window/listwindow.hpp"
 #include "window/playlistwindow.hpp"
 
 using namespace Ui;
@@ -82,6 +83,13 @@ Screen::Screen(Main::Settings & settings, Mpc::Client & client, Ui::Search const
    mainWindows_[Console]  = new Ui::ConsoleWindow (*this);
    mainWindows_[Library]  = new Ui::LibraryWindow (settings, *this, client, search);
    mainWindows_[Browse]   = new Ui::BrowseWindow  (settings, *this, client, search);
+
+#if LIBMPDCLIENT_CHECK_VERSION(2,5,0)
+   mainWindows_[Lists]    = new Ui::ListWindow    (settings, *this, client, search);
+#else
+   mainWindows_[Lists]    = NULL;
+#endif
+
    mainWindows_[Playlist] = new Ui::PlaylistWindow(settings, *this, client, search);
    statusWindow_          = newwin(1, maxColumns_, maxRows_ + 1, 0);
    tabWindow_             = newwin(1, maxColumns_, 0, 0);
@@ -89,7 +97,10 @@ Screen::Screen(Main::Settings & settings, Mpc::Client & client, Ui::Search const
    // Mark them all as visible
    for (uint32_t i = 0; i < MainWindowCount; ++i)
    {
-      visibleWindows_.push_back(static_cast<MainWindow>(i));
+      if (mainWindows_[i] != NULL)
+      {
+         visibleWindows_.push_back(static_cast<MainWindow>(i));
+      }
    }
 
    // Commands must be read through a window that is always visible
@@ -99,8 +110,6 @@ Screen::Screen(Main::Settings & settings, Mpc::Client & client, Ui::Search const
    // Window setup
    mainWindows_[Console]->SetAutoScroll(true);
    SetStatusLine("%s", "");
-
-   ENSURE(WindowsAreInitialised() == true);
 
    SetVisible(Console, false);
 }
@@ -129,11 +138,12 @@ Screen::~Screen()
    if (windowTable.size() != MainWindowCount)
    {
       // Names for each of the windows
-      windowTable["help"]     = Help;
-      windowTable["console"]  = Console;
-      windowTable["library"]  = Library;
-      windowTable["browse"]   = Browse;
-      windowTable["playlist"] = Playlist;
+      windowTable["help"]      = Help;
+      windowTable["console"]   = Console;
+      windowTable["lists"]     = Lists;
+      windowTable["library"]   = Library;
+      windowTable["browse"]    = Browse;
+      windowTable["playlist"]  = Playlist;
    }
 
    WindowTable::const_iterator it = windowTable.find(name);
@@ -161,6 +171,7 @@ Screen::~Screen()
       // Values for each of the windows
       windowTable[Help]     = "help";
       windowTable[Console]  = "console";
+      windowTable[Lists]    = "lists";
       windowTable[Library]  = "library";
       windowTable[Browse]   = "browse";
       windowTable[Playlist] = "playlist";
@@ -406,7 +417,10 @@ void Screen::Redraw() const
 
 void Screen::Redraw(MainWindow window) const
 {
-   mainWindows_[window]->Redraw();
+   if (mainWindows_[window] != NULL)
+   {
+      mainWindows_[window]->Redraw();
+   }
 }
 
 bool Screen::Resize(bool forceResize)
@@ -451,8 +465,11 @@ bool Screen::Resize(bool forceResize)
 
       for (int i = 0; (i < MainWindowCount); ++i)
       {
-         wclear(mainWindows_[i]->N_WINDOW());
-         mainWindows_[i]->Resize(maxRows_, maxColumns_);
+         if (mainWindows_[i] != NULL)
+         {
+            wclear(mainWindows_[i]->N_WINDOW());
+            mainWindows_[i]->Resize(maxRows_, maxColumns_);
+         }
       }
    }
 
@@ -567,45 +584,51 @@ void Screen::SetActiveWindow(Skip skip)
 
 void Screen::SetVisible(MainWindow window, bool visible)
 {
-   //! \todo Handle the case when there is no visible tabs left and clear the mainwindow
-   bool found = false;
-
-   if ((window == window_) && (visible == false))
+   if (mainWindows_[window] != NULL)
    {
-      SetActiveWindow(Ui::Screen::Next);
-   }
+      //! \todo Handle the case when there is no visible tabs left and clear the mainwindow
+      bool found = false;
 
-   if (window < MainWindowCount)
-   {
-      for (std::vector<MainWindow>::iterator it = visibleWindows_.begin(); ((it != visibleWindows_.end()) && (found == false)); ++it)
+      if ((window == window_) && (visible == false))
       {
-         if ((*it) == window)
-         {
-            found = true;
-
-            if (visible == false)
-            {
-               visibleWindows_.erase(it);
-            }
-         }
+         SetActiveWindow(Ui::Screen::Next);
       }
 
-      if ((found == false) && (visible == true))
+      if (window < MainWindowCount)
       {
-         visibleWindows_.push_back(window);
+         for (std::vector<MainWindow>::iterator it = visibleWindows_.begin(); ((it != visibleWindows_.end()) && (found == false)); ++it)
+         {
+            if ((*it) == window)
+            {
+               found = true;
+
+               if (visible == false)
+               {
+                  visibleWindows_.erase(it);
+               }
+            }
+         }
+
+         if ((found == false) && (visible == true))
+         {
+            visibleWindows_.push_back(window);
+         }
       }
    }
 }
 
 void Screen::SetActiveAndVisible(MainWindow window)
 {
-   SetVisible(window, true);
-
-   for (uint32_t i = 0; i < visibleWindows_.size(); ++i)
+   if (mainWindows_[window] != NULL)
    {
-      if (visibleWindows_.at(i) == window)
+      SetVisible(window, true);
+
+      for (uint32_t i = 0; i < visibleWindows_.size(); ++i)
       {
-         SetActiveWindow(i);
+         if (visibleWindows_.at(i) == window)
+         {
+            SetActiveWindow(i);
+         }
       }
    }
 }
@@ -617,31 +640,34 @@ void Screen::MoveWindow(uint32_t position)
 
 void Screen::MoveWindow(MainWindow window, uint32_t position)
 {
-   bool found = false;
-
-   uint32_t pos = 0;
-
-   if (position >= visibleWindows_.size())
+   if (mainWindows_[window] != NULL)
    {
-      position = visibleWindows_.size() - 1;
-   }
+      bool found = false;
 
-   for (std::vector<MainWindow>::iterator it = visibleWindows_.begin(); ((it != visibleWindows_.end()) && (found == false)); ++it)
-   {
-      if ((*it) == window)
+      uint32_t pos = 0;
+
+      if (position >= visibleWindows_.size())
       {
-         found = true;
-         visibleWindows_.erase(it);
+         position = visibleWindows_.size() - 1;
       }
-   }
 
-   std::vector<MainWindow>::iterator it;
+      for (std::vector<MainWindow>::iterator it = visibleWindows_.begin(); ((it != visibleWindows_.end()) && (found == false)); ++it)
+      {
+         if ((*it) == window)
+         {
+            found = true;
+            visibleWindows_.erase(it);
+         }
+      }
 
-   for (it = visibleWindows_.begin(); ((it != visibleWindows_.end()) && (pos < position)); ++it, ++pos) { }
+      std::vector<MainWindow>::iterator it;
 
-   if (pos == position)
-   {
-      visibleWindows_.insert(it, window);
+      for (it = visibleWindows_.begin(); ((it != visibleWindows_.end()) && (pos < position)); ++it, ++pos) { }
+
+      if (pos == position)
+      {
+         visibleWindows_.insert(it, window);
+      }
    }
 }
 
@@ -739,19 +765,6 @@ void Screen::UpdateTabWindow() const
 
    wattroff(tabWindow_, A_UNDERLINE);
    wrefresh(tabWindow_);
-}
-
-
-bool Screen::WindowsAreInitialised()
-{
-   bool result = true;
-
-   for (int i = 0; ((i < MainWindowCount) && (result == true)); ++i)
-   {
-      result = (mainWindows_[i] != NULL);
-   }
-
-   return result;
 }
 
 

commit 92a4438b83d17473b9d2a34c6247dcc297b81df1

Fix bug introduced in 17663d635530b77acb4b984178e20f15bd0bab09

src/mpdclient.hpp
@@ -231,8 +231,8 @@ namespace Mpc
             mpd_playlist_free(nextPlaylist);
          }
       }
-   }
 #endif
+   }
 }
 
 #endif