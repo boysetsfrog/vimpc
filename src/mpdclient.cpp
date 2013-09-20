6fe53b1ea4537ffe6a002988c3065259861b7825
Nathan Sweetman <nathan.sweetman@gmail.com>
2013-09-20 10:39:19 +0930

    Reduced time that is used to bunch client commands

--- src/mpdclient.cpp
+++ src/mpdclient.cpp
@@ -1513,7 +1513,7 @@ void Client::ClientQueueExecutor(Mpc::Client * client)
             std::unique_lock<std::mutex> Lock(QueueMutex);
 
             if ((Queue.empty() == false) ||
-                (Condition.wait_for(Lock, std::chrono::milliseconds(250)) != std::cv_status::timeout))
+                (Condition.wait_for(Lock, std::chrono::milliseconds(100)) != std::cv_status::timeout))
             {
                if (Queue.empty() == false)
                {
