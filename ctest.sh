59e9c3b9423b6331657c7beef9e2049b94816894
Nathan Sweetman <nathan.sweetman@gmail.com>
2013-11-11 18:08:59 +1030

    Added basic script (will require manual editting at the moment) to test compilations with different compilers and flags

---
+++ ctest.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Debian compilers
+GCC46=/usr/bin/g++-4.6
+GCC46_LIB=/usr/lib/gcc/i486-linux-gnu/4.6/
+GCC47=/usr/bin/g++-4.7
+GCC47_LIB=/usr/lib/gcc/i486-linux-gnu/4.7/
+
+# Colours
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
+col=$(($(tput cols) - 8))
+
+compile_test ()
+{
+    FLAGS=$@
+
+    echo ""
+    echo "Running configure $FLAGS... "
+
+    RESULT=`eval ./configure $FLAGS 2>&1`
+
+    if [ $? -eq 0 ]
+    then
+        printf '%*s%s%s' $col "[ $GREEN" "OK" "$NORMAL ]"
+        echo ""
+        echo "Running make... "
+        make clean >/dev/null 2>&1
+        RESULT=`make -j4 2>&1`
+
+        if [ $? -eq 0 ]
+        then
+            printf '%*s%s%s' $col "[ $GREEN" "OK" "$NORMAL ]"
+            echo ""
+        else
+            printf '%*s%s%s' $col "[ $RED" "FAIL" "$NORMAL ]"
+            echo ""
+            echo "DUMPING OUTPUT"
+            echo "===================="
+            echo "$RESULT"
+            echo "===================="
+        fi
+    else
+        printf '%*s%s%s' $col "[ $RED" "FAIL" "$NORMAL ]"
+        echo ""
+        echo "DUMPING OUTPUT"
+        echo "===================="
+        echo "$RESULT"
+        echo "===================="
+    fi
+}
+
+compile_sets ()
+{
+    compile_test
+    compile_test --enable-debug=yes --enable-test=yes
+    compile_test --enable-boost=yes
+    compile_test --enable-taglib=no
+}
+
+echo "Default compiler..."
+compile_sets
+
+echo "Default g++ as compiler..."
+export CXX=g++ 
+compile_sets
+
+echo "g++ 4.6 as compiler..."
+export CXX=$GCC46 LDFLAGS="-L$GCC46_LIB" 
+compile_sets
+
+echo "g++ 4.7 as compiler..."
+export CXX=$GCC47 LDFLAGS="-L$GCC47_LIB" 
+compile_sets
+
+echo "clang++ as compiler..."
+export CC=clang CXX=clang++ 
+compile_sets
+
+echo "clang++ with libc++ as compiler..."
+export CC=clang CXX=clang++ CXXFLAGS="-stdlib=libc++" LDFLAGS="-lc++abi"
+compile_sets
