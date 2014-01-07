8bffc3d5a6f62422a983aaa56ada337f2206bcd5
Nathan Sweetman <nathan.sweetman@gmail.com>
2014-01-07 15:17:16 +1030

    Show URI in status bar if song title is unknown

--- src/clientstate.cpp
+++ src/clientstate.cpp
@@ -311,13 +311,22 @@ void ClientState::DisplaySongInformation()
       {
          char const * const cartist  = mpd_song_get_tag(currentSong_, MPD_TAG_ARTIST, 0);
          char const * const ctitle   = mpd_song_get_tag(currentSong_, MPD_TAG_TITLE, 0);
+         char const * const curi     = mpd_song_get_uri(currentSong_);
          uint32_t     const duration = mpd_song_get_duration(currentSong_);
          uint32_t     const elapsed  = elapsed_;
          uint32_t     const remain   = (duration > elapsed) ? duration - elapsed : 0;
-         std::string  const artist   = (cartist != NULL) ? cartist : "";
+         std::string  const artist   = (cartist != NULL) ? cartist : "Unknown";
          std::string  const title    = (ctitle != NULL) ? ctitle : "";
+         std::string  const uri      = (curi != NULL) ? curi : "";
 
-         screen_.SetStatusLine("[%5u] %s - %s", GetCurrentSongPos() + 1, artist.c_str(), title.c_str());
+         if (title != "")
+         {
+            screen_.SetStatusLine("[%5u] %s - %s", GetCurrentSongPos() + 1, artist.c_str(), title.c_str());
+         }
+         else
+         {
+            screen_.SetStatusLine("[%5u] %s", GetCurrentSongPos() + 1, uri.c_str());
+         }
 
          if (settings_.Get(Setting::TimeRemaining) == false)
          {
